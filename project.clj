68d4f327271f710c0800d7e552b444a0966864d0
mikera <mike.r.anderson.13@gmail.com>
2015-12-05 10:15:49 +0800

    update vectorz dep

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A neural network toolkit for Clojure."
   :dependencies [[org.clojure/clojure "1.8.0-RC3"]
                  [com.taoensso/timbre "4.1.4"]
-                 [net.mikera/vectorz-clj "0.37.0"]]
+                 [net.mikera/vectorz-clj "0.38.0"]]
 
   :jvm-opts  ["-Xmx8g"
               "-XX:+UseConcMarkSweepGC"
