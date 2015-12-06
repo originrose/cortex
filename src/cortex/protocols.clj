2f59c0a22443030e39e5ef11038994422c6e123e
mikera <mike.r.anderson.13@gmail.com>
2015-12-06 13:35:08 +0800

    Add PModule protocol

--- src/cortex/protocols.clj
+++ src/cortex/protocols.clj
@@ -1,2 +1,10 @@
-(ns cortex.protocols)
+(ns cortex.protocols
+  "Protocols for cortex ML Module implementations")
 
+(defprotocol PModule
+  "Protocol for a generic module. All cortex modules must implement this."
+  (calc [m input]
+    "Performs module calculation, returning an updated module that includes the output and
+     any intermediate states computed")
+  (output [m]
+    "Returns the calculated output of a module"))
