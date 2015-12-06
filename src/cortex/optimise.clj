4c93c3ac80d1ffe632fb4ab852bd2dae42ff771f
mikera <mike.r.anderson.13@gmail.com>
2015-12-06 18:59:13 +0800

    add optimise namespace

---
+++ src/cortex/optimise.clj
@@ -0,0 +1,6 @@
+(ns cortex.optimise
+  "Namespace for optimisation algorithms and optimiser objects"
+  (:require [cortex.protocols :as cp])
+  (:require [cortex.util :as util :refer [error]])
+  (:require [clojure.core.matrix :as m]))
+
