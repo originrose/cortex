71653435c2d80ce03ef9e32ac979ab4899305665
mikera <mike.r.anderson.13@gmail.com>
2015-12-06 14:26:09 +0800

    Add test for empty stack

--- test/cortex/wiring_test.clj
+++ test/cortex/wiring_test.clj
@@ -7,7 +7,12 @@
     (is (= 2 (output (calc m 1))))))
 
 (deftest test-stack-module
+  ;; simple 2-layer stack
   (let [m (function-module inc)
         st (stack-module [m m])]
-    (is (= 3 (output (calc st 1))))))
+    (is (= 3 (output (calc st 1)))))
+  
+  ;; empty stack
+  (let [st (stack-module [])]
+    (is (= 1 (output (calc st 1))))))
 
