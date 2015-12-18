0836d2fa32d43db56f51a34b98e6ad895225d1fd
Mike <mike@mikera.net>
2015-12-18 22:44:54 +0000

    Update deps

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject thinktopic/cortex "0.1.0-SNAPSHOT"
   :description "A neural network toolkit for Clojure."
-  :dependencies [[org.clojure/clojure "1.8.0-RC3"]
+  :dependencies [[org.clojure/clojure "1.8.0-RC4"]
                  [com.taoensso/timbre "4.1.4"]
                  [net.mikera/vectorz-clj "0.38.0"]
                  [org.clojure/test.check "0.9.0"]]
