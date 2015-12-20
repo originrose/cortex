413344304ac3cf6dfa06b6d118e32f07a45d15f4
Mike <mike@mikera.net>
2015-12-20 06:48:06 +0000

    Update to latest vectorz-clj

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A neural network toolkit for Clojure."
   :dependencies [[org.clojure/clojure "1.8.0-RC4"]
                  [com.taoensso/timbre "4.1.4"]
-                 [net.mikera/vectorz-clj "0.38.0"]
+                 [net.mikera/vectorz-clj "0.39.0"]
                  [org.clojure/test.check "0.9.0"]]
 
   :jvm-opts  ["-Xmx8g"
