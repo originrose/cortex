003c86aa321b45e9ef06832a36356118fa9ccc40
Charles Gruenwald <charles@thinktopic.com>
2015-12-20 18:44:49 -0700

    [examples/scrabble] Re-shape the input / output to look like the "Machine Learning Live" talk. Add documentation.

--- examples/scrabble/src/scrabble/core.clj
+++ examples/scrabble/src/scrabble/core.clj
@@ -19,37 +19,68 @@
                       \l 1 \m 3 \n 1 \o 1 \p 3 \q 10 \r 1 \s 1 \t 1 \u 1 \v 4
                       \w 4 \x 8 \y 4 \z 10})
 
-(defn run-experiment []
-  (let [;; convert the values from the scrabble pieces into 1x1 matrices
-        training-data   (mapv #(into [] [[(- (int %) (int \a))]]) (keys scrabble-values))
+;; coders
 
-        ;; convert the "labels" of the scrabble pieces into 1x1 matrices
-        training-labels (mapv #(into [] [[%]])                    (vals scrabble-values))
+(defn num->bit-array [input & {:keys [bits]
+                               :or {bits 4}}]
+  "Takes a number, returns a vector of floats representing the number in
+  binary. Optionally specify the number of bits for padding."
+ (->> (clojure.pprint/cl-format nil (str "~" bits ",'0',B") input)
+      (mapv #(new Double (str %)))))
 
-        ;; This defines the shape of the network
-        ;; TODO: add some explanation for the intution of the size of the various layers
-        network (net/sequential-network
-                  [(net/linear-layer :n-inputs 1 :n-outputs 10)
-                   (net/sigmoid-activation 10)
-                   (net/linear-layer :n-inputs 10 :n-outputs 1)])
+(defn char->bit-array [character]
+  "Takes a character and bit vector with a 1.0 in the nth position where
+  n is the offset from the character 'a'."
+  (let [offset (- (int character) (int \a))
+        begin (take offset (repeatedly #(new Double 0.0)))
+        end (take (- 26 offset 1) (repeatedly #(new Double 0.0)))]
+  (into [] (concat begin [1.0] end))))
 
-        n-epochs 10000            ;; Number of iterations to train
+;; training
+
+(defn train [network training-data training-labels]
+  (let [n-epochs 2000            ;; Number of iterations to train
         learning-rate 0.3
         momentum 0.9
         batch-size 1
         loss-fn (opt/quadratic-loss)
         optimizer (net/sgd-optimizer network loss-fn learning-rate momentum)]
+    (println "Training network...")
+    (net/train-network optimizer n-epochs batch-size training-data training-labels)))
+
+(defn evaluate [network test-data test-labels]
+  (let [_ (println "Evaluating network...")
+        [results score] (net/evaluate network test-data test-labels)
+        label-count (count test-data)
+        score-percent (float (/ score label-count))]
+    (println (format "Score: %f [%d of %d]" score-percent score label-count))))
+
+(defn run-experiment []
+  (let [;; convert the values from the scrabble pieces (letters) into floating-point bit-arrays
+        training-data   (mapv #(into [] [(char->bit-array %)]) (keys scrabble-values))
+
+        ;; convert the "labels" of the scrabble pieces (scores) into floating-point bit-arrays
+        training-labels (mapv #(into [] [(num->bit-array %)]) (vals scrabble-values))
+
+        ;; infer the width of the input and output based on the shape of the training data
+        ;; - input layer has one input corresponding to each possible letter
+        ;; - output layer is of size 4 as we can represent the corresponding scores for
+        ;;   each letter with 4 bits (max score of 10)
+        input-width (last (mat/shape training-data))
+        output-width (last (mat/shape training-labels))
+
+        ;; This defines the shape of the network
+        ;; TODO: add some explanation for the intution of the size of the hidden layer
+        network (net/sequential-network
+                  [(net/linear-layer :n-inputs input-width :n-outputs 6)
+                   (net/sigmoid-activation 6)
+                   (net/linear-layer :n-inputs 6 :n-outputs output-width)])]
 
     ;; Run the training portion of the algorithm using the parameters from above
-    (println "Training network...")
-    (net/train-network optimizer n-epochs batch-size training-data training-labels)
+    (train network training-data training-labels)
 
-    ;; Once the network has been evaluated, use the same input data to test to see
+    ;; Once the network has been trained, use the same input data to test to see
     ;; how the neural network has learned the target function. We use the same
     ;; data for testing as training for this degenerate case.
-    (println "Evaluating network...")
-    (let [[results score] (net/evaluate network training-data training-labels)
-          label-count (count training-data)
-          score-percent (float (/ score label-count))]
-      (println (format "Score: %f [%d of %d]" score-percent score label-count)))))
+    (evaluate network training-data training-labels)))
 
