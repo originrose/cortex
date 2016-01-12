093ed7738a8c8caaebbdd288d97a442357603985
mikera <mike.r.anderson.13@gmail.com>
2016-01-12 13:07:45 +0800

    Update to latest Clojure RC5

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject thinktopic/cortex "0.1.0-SNAPSHOT"
   :description "A neural network toolkit for Clojure."
-  :dependencies [[org.clojure/clojure "1.8.0-RC4"]
+  :dependencies [[org.clojure/clojure "1.8.0-RC5"]
                  [com.taoensso/timbre "4.2.0"]
                  [net.mikera/vectorz-clj "0.40.0"]
                  [org.clojure/test.check "0.9.0"]]
