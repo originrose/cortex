15c9a49f0c51d666250e0c021e2c8516cda4a9ce
mikera <mike.r.anderson.13@gmail.com>
2016-01-23 11:52:46 +0800

    Test emulation of logistic function using function module

--- test/cortex/function_test.clj
+++ test/cortex/function_test.clj
@@ -28,6 +28,18 @@
         (let [bm (backward fm input [10 10 -10])]
           (is (m/equals [0 2.5 0] (input-gradient bm))))))))
 
+(deftest test-function
+  (testing "emulated logistic function"
+    (let [m (layers/logistic [3])
+          m2 (layers/function m/logistic (fn [input output] (m/mul output (m/sub 1.0 output))))]
+      (let [v [-10 1 10]]
+        (is (m/equals (calc-output m v) (calc-output m2 v))))
+      (let [input [-10 1 10]
+            target [0.1 0.5 0.6]
+            m (train m input target)
+            m2 (train m2 input target)]
+        (is (m/equals (input-gradient m) (input-gradient m2)))))))
+
 (deftest test-linear-module
   (testing "Parameters"
     (let [wm (layers/linear [[1 2] [3 4]] [0 10])
