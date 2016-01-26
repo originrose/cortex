0fe8754ed0766386e9588b7da3fe2f51295b71d5
Charles Gruenwald <charles@thinktopic.com>
2016-01-26 16:09:40 -0700

    [examples/scrabble] Update to newer version of clojure.

--- examples/scrabble/project.clj
+++ examples/scrabble/project.clj
@@ -1,4 +1,4 @@
 (defproject scrabble "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [thinktopic/cortex "0.1.0-SNAPSHOT"]]
   :main scrabble.core)
