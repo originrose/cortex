380f94bbe09187c43e58533ace25aa60f1c99925
mikera <mike.r.anderson.13@gmail.com>
2016-01-28 16:47:50 +0800

    tests for scale layers

--- test/cortex/layers_test.clj
+++ test/cortex/layers_test.clj
@@ -11,5 +11,12 @@
             [clojure.test.check.clojure-test :refer (defspec)]))
 
 (deftest test-scale
-  (let [a (layers/scale [2] [1 2] [30 40])]
-    (is (m/equals [31 42] (calc-output a [1 1])))))
+  (testing "array valued scale and constant"
+    (let [a (layers/scale [2] [1 2] [30 40])]
+     (is (m/equals [31 42] (calc-output a [1 1])))))
+  (testing "scalar scale and constant"
+    (let [a (layers/scale [2] 2 10)]
+      (is (m/equals [12 14] (calc-output a [1 2])))))
+  (testing "matrix values constant"
+    (let [a (layers/scale [2 2] nil 1)]
+      (is (m/equals [[2 3] [4 5]] (calc-output a [[1 2] [3 4]]))))))
