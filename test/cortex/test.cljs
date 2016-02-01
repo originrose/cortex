769898f855d3de2143f1b64d6312309663720648
Charles Gruenwald <charles@thinktopic.com>
2016-02-01 14:53:43 -0700

    [tests/noraliser-test] Add the normalizer test.

--- test/cortex/test.cljs
+++ test/cortex/test.cljs
@@ -2,11 +2,13 @@
   (:require [doo.runner :refer-macros [doo-tests]]
             [cortex.wiring-test]
             [cortex.function-test]
-            [cortex.network-test]))
+            [cortex.network-test]
+            [cortex.normaliser-test]))
 
 (enable-console-print!)
 
 (doo-tests 'cortex.wiring-test
            'cortex.function-test
-           'cortex.network-test)
+           'cortex.network-test
+           'cortex.normaliser-test)
 
