a1d01f9daf9e5fd5d43c5312474ec27686a37f50
Charles Gruenwald <charles@thinktopic.com>
2016-02-02 14:44:31 -0700

    [tests] Enable all the tests.

--- test/cortex/test.cljs
+++ test/cortex/test.cljs
@@ -3,12 +3,16 @@
             [cortex.wiring-test]
             [cortex.function-test]
             [cortex.network-test]
-            [cortex.normaliser-test]))
+            [cortex.normaliser-test]
+            [cortex.optimise-test]
+            [cortex.util-test]))
 
 (enable-console-print!)
 
 (doo-tests 'cortex.wiring-test
            'cortex.function-test
+           'cortex.util-test
            'cortex.network-test
-           'cortex.normaliser-test)
+           'cortex.normaliser-test
+           'cortex.optimise-test)
 
