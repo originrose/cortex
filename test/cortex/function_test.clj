30f90232bd7cd4a5d53d378a61ba31c0ad38b146
Chris Nuernberger <chris@thinktopic.com>
2016-02-02 13:50:26 -0700

    Added a small test for the backward pass of the linear layer

--- test/cortex/function_test.clj
+++ test/cortex/function_test.clj
@@ -13,27 +13,27 @@
       (is (m/equals [0 0 0] (output m)))
       (let [cm (calc m [-1000 0 1000])]
         (is (m/equals [0 0.5 1] (output cm))))))
-  
+
   (testing "logistic applied to scalars"
     (let [m (layers/logistic [])]
       (let [cm (calc m 0)]
         (is (m/equals 0.5 (output cm))))))
-  
+
   (testing "forward and backward pass"
     (let [m (layers/logistic [3])]
-      
+
       (let [input [-1000 0 1000]
             fm (forward m input)]
         (is (m/equals [0 0.5 1] (output fm)))
-        
+
         (let [bm (backward fm input [10 10 -10])]
           (is (m/equals [0 2.5 0] (input-gradient bm))))))))
 
 (deftest test-function
   (testing "emulated logistic function"
     (let [m (layers/logistic [3])
-          m2 (layers/function m/logistic 
-                              (fn [input output-gradient] 
+          m2 (layers/function m/logistic
+                              (fn [input output-gradient]
                                 (m/mul output-gradient (util/logistic' (m/logistic input)))))]
       (let [v [-10 1 10]]
         (is (m/equals (calc-output m v) (calc-output m2 v))))
@@ -56,9 +56,14 @@
       (is (m/equals [1 2 3 4 0 10] parm))
       (is (= (m/shape parm) (m/shape grad)))
       (is (m/zero-matrix? grad))))
-  
+
   (testing "Calculation"
     (let [wm (layers/linear [[1 2] [3 4]] [0 10])
           wm (calc wm [1 2])]
-      (is (m/equals [5 21] (output wm))))))
-
+      (is (m/equals [5 21] (output wm)))))
+  (testing "Backward pass"
+    (let [wm (layers/linear [[1 2] [3 4]] [0 10])
+          wm (backward wm [1 2] [1 2])]
+      (is (m/equals [1 2 2 4] (:weight-gradient wm)))
+      (is (m/equals [1 2] (:bias-gradient wm)))
+      (is (m/equals [7 10] (:input-gradient wm))))))
