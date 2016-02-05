1879b459e7aa479ede594e1f23f2cc3c678c611b
Chris Nuernberger <chris@thinktopic.com>
2016-02-05 10:52:55 -0700

    Removed the protobuf library because the javac step conflicts with clojurescript

--- project.clj
+++ project.clj
@@ -5,14 +5,12 @@
                  [net.mikera/vectorz-clj "0.43.0"]
                  [org.clojure/test.check "0.9.0"]
                  [thinktopic/matrix.fressian "0.2.1"]
-                 [com.google.protobuf/protobuf-java "2.6.1"]]
+                 [caffe-protobuf "0.1.0"]]
 
   :profiles {:dev {:dependencies [[net.mikera/cljunit "0.3.1"]]
                    :java-source-paths ["test"]}}
 
   :source-paths ["src" "test"]
-
-  :java-source-paths ["java"]
 
   :resource-paths ["resources"]
 
