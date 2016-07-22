28c3de5ce7045f71f1445f58a1ae468909e13b43
Radon Rosborough <radon.neon@gmail.com>
2016-07-22 12:39:33 -0600

    Add tests for optimise.parameters

---
+++ cortex/test/cljc/cortex/optimise/parameters_test.clj
@@ -0,0 +1,15 @@
+(ns cortex.optimise.parameters-test
+  (:require [clojure.test :refer :all]
+            [cortex.nn.protocols :as cp]
+            [cortex.optimise.parameters :refer :all]))
+
+(def sample-map
+  {:state {:params [1 2 3]
+           :data [:a :b :c]}})
+
+(deftest protocol-extension-test
+  (is (= (cp/parameters sample-map)
+         [1 2 3]))
+  (is (= (cp/update-parameters sample-map [4 5 6])
+         {:state {:params [4 5 6]
+                  :data [:a :b :c]}})))
