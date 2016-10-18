1a0d58c2ef0548388b07d58d4b02b7bce30ce0a0
Chris Nuernberger <chris@thinktopic.com>
2016-10-18 17:22:42 -0600

    Uggh.  In the middle of reshaping all matrixesemacs-nw src/think/cortex/keras/core.clj !

--- keras/src/think/cortex/keras/core.clj
+++ keras/src/think/cortex/keras/core.clj
@@ -6,7 +6,8 @@
             [clojure.java.io :as io]
             [clojure.core.matrix :as m]
             [clojure.core.matrix.macros :refer [c-for]]
-            [think.compute.datatype :as dtype]))
+            [think.compute.datatype :as dtype]
+            [clojure.string :as string]))
 
 
 (defn read-model
@@ -27,11 +28,14 @@
         activation (keyword (get config :activation))
         conv-desc (first (desc/convolutional-expanded kernel-x kernel-y pad-x pad-y
                                                       stride-x stride-y kernel-count))
-        conv-desc (assoc conv-desc :id id)]
+        conv-desc (assoc conv-desc
+                         :id id)
+        ]
     (when-not (= (:dim_ordering config) "tf")
       (throw
        (Exception. "Please convert model to 'tf' weights.  'th' weights are not supported.")))
-    (if activation
+    (if (and activation
+             (not= activation :linear))
       [conv-desc {:type activation}]
       [conv-desc])))
 
@@ -58,7 +62,9 @@
   [{:keys [config]}]
   (let [output-size (long (:output_dim config))
         activation (keyword (get config :activation "linear"))
-        retval (first (desc/linear output-size))]
+        retval (-> (first (desc/linear output-size))
+                   (assoc :id (keyword (:name config))))
+        ]
     (if-not (= activation :linear)
       [retval {:type activation}]
       [retval])))
@@ -86,7 +92,7 @@
      (flatten (concat (desc/input width height n-channels)
                       (mapv model-item->desc model-vector))))))
 
-(defn- hdf5-child-map
+(defn hdf5-child-map
   [node]
   (into {} (map (fn [node-child]
                   [(keyword (hdf5/get-name node-child))
@@ -109,14 +115,80 @@
                                                  (get dest row) 0 n-cols)))))))
 
 
+(defn ensure-doubles
+  ^doubles [data]
+  (if (not= :double (dtype/get-datatype data))
+    (let [double-data (double-array (m/ecount data))]
+      (dtype/copy! data 0 double-data 0 (m/ecount data))
+      double-data)
+    data))
+
+(defn- dims->strides
+  [dims]
+  (vec (reduce (fn [retval next-dim]
+                 (let [last-stride (or (first retval) 1)
+                       next-dim (or next-dim 1)]
+                   (conj retval (* last-stride next-dim))))
+               ()
+               (reverse dims))))
+
+
+(defn- strides-idx->dim-indexes
+  [strides ^long idx]
+  (let [num-strides (count strides)]
+   (loop [retval []
+          leftover idx
+          stride-idx 0]
+     (if (< stride-idx num-strides)
+       (let [stride (strides stride-idx)
+             next-item (quot leftover stride)
+             next-leftover (rem leftover stride)]
+         (recur (if-not (= 0 stride-idx)
+                  (conj retval next-item)
+                  retval) next-leftover (inc stride-idx)))
+       (conj retval leftover)))))
+
+
+(defn- strides-dim-indexes->idx
+  ^long [strides dim-indexes]
+  (let [n-elems (count strides)]
+    (loop [retval 0
+           idx 0]
+      (if (< idx n-elems)
+        (recur (+ retval (* (long (if (= idx (- n-elems 1))
+                                    1
+                                    (strides (inc idx))))
+                            (long (dim-indexes idx))))
+               (inc idx))
+        retval))))
+
+
+(defn- input-idx->output-idx
+  ^long [input-idx input-strides reshape-indexes output-strides]
+  (let [input-dim-indexes (strides-idx->dim-indexes input-strides input-idx)
+        output-dim-indexes (mapv input-dim-indexes reshape-indexes)]
+    (strides-dim-indexes->idx output-strides output-dim-indexes)))
+
+
+(defn- reshape-data
+  "Given input with given dims and relative reshape indexes
+produce a new array of double values in the order desired"
+  ^doubles [data data-dims reshape-indexes]
+  (let [^doubles data (ensure-doubles data)
+        n-elems (long (reduce * data-dims))
+        retval (double-array (alength data))
+        input-strides (dims->strides data-dims)
+        output-dims (mapv data-dims reshape-indexes)
+        output-strides (dims->strides output-dims)]
+    (c-for [idx 0 (< idx n-elems) (inc idx)]
+           (let [output-idx (input-idx->output-idx idx input-strides reshape-indexes output-strides)]
+             (aset retval output-idx (aget data idx))))
+    retval))
+
+
 (defn to-core-matrix
   [data ideal-shape]
-  (let [^doubles data (if (not= :double (dtype/get-datatype data))
-                        (let [double-data (double-array (m/ecount data))]
-                          (dtype/copy! data 0 double-data 0 (m/ecount data))
-                          double-data)
-                        data)]
-
+  (let [^doubles data (ensure-doubles data)]
     ;;https://github.com/mikera/core.matrix/issues/299
 
     ;;The simple case of using m/reshape has serious performance issues.
@@ -141,6 +213,10 @@
   [(:output-size desc)
    (quot (m/ecount weights-raw-data) (:output-size desc))])
 
+(defn built-desc->keras-dims
+  [built-desc]
+  (if (= (:type built-desc) :convolutional)))
+
 (defn- load-weights
   [desc-seq weight-file]
   (let [weight-entry (first (filter (fn [node]
@@ -150,8 +226,7 @@
         _ (when-not weight-entry
             (throw (Exception. "Weight file does not appear to contain model_weights.")))
         node-map (hdf5-child-map weight-entry)]
-    (println node-map)
-    (mapv (fn [desc]
+    (mapv (fn [[desc built-desc]]
             (let [weight-node (get node-map (:id desc))]
               (if (and weight-node (seq (hdf5/get-children weight-node)))
                 (let [weight-map (hdf5-child-map weight-node)
@@ -160,18 +235,27 @@
                       weight-id (keyword (str (name (:id desc)) "_W"))
                       bias-id (keyword (str (name (:id desc)) "_b"))
                       weight-ds (get weight-map weight-id)
-                      bias-ds (get weight-map bias-id)]
+                      bias-ds (get weight-map bias-id)
+                      [weight-ds bias-ds] (if (and weight-ds bias-ds)
+                                            [weight-ds bias-ds]
+                                            (let [children (hdf5/get-children weight-node)]
+                                              [(first children) (second children)]))]
                   (when-not (and weight-ds bias-ds)
                     (throw (Exception.
                             (format "Failed to find weights and bias: wanted %s, found %s"
                                     [weight-id bias-id] (keys weight-map)))))
                   (println "loading weights/bias for" (:id desc))
-                  (let [weight-raw-data (:data (hdf5/->clj weight-ds))
-                        weight-shape (get-weight-shape desc weight-raw-data)]
+                  (let [weight-clj (hdf5/->clj weight-ds)
+                        weight-raw-data (:data weight-clj)
+                        weight-shape (get-weight-shape desc weight-raw-data)
+                        weight-double-data (if-let [keras-dims (built-desc->keras-dims built-desc)]
+                                             ;;Keras dimensions are: 3 height 2 width 1 n-channels 0 n-filters
+                                             ;;We want: n-filters n-channels height width
+                                             (reshape-data weight-raw-data keras-dims [0 1 3 2])
+                                             (ensure-doubles weight-raw-data))]
                     (assoc desc
-                           :weights (to-core-matrix weight-raw-data weight-shape)
-                           :bias (to-core-matrix (:data (hdf5/->clj bias-ds))
-                                                 [(second weight-shape)]))))
+                           :weights (to-core-matrix weight-double-data weight-shape)
+                           :bias (ensure-doubles (:data (hdf5/->clj bias-ds))))))
                 desc)))
           desc-seq)))
 
@@ -179,7 +263,7 @@
 (defn load-weights-for-description
   [desc-seq weights-fname]
   (resource/with-resource-context
-    (load-weights (desc-seq (hdf5/open-file weights-fname)))))
+    (load-weights desc-seq (hdf5/open-file weights-fname))))
 
 
 (defn model->description
@@ -189,21 +273,98 @@
       model->simple-description
       (load-weights-for-description weight-hdf5-fname)))
 
+(defn tokenize-output-name
+  [out-name]
+  (let [parts
+        (string/split out-name #"_")]
+    (if (= (count parts) 4)
+      {:index (Long/parseLong (parts 1))
+       :layer-type (keyword (parts 3))}
+      (throw (Exception. (format "fixme: %s" out-name))))))
+
+(defn layer-output->ordered-data
+  [layer-outputs]
+  (->> (hdf5-child-map layer-outputs)
+       (mapv (fn [[name-keywd node]]
+               (let [{:keys [index layer-type] } (tokenize-output-name (name name-keywd))
+                     clj-node (hdf5/->clj node)
+                     node-data (:data clj-node)]
+                 [index {:layer-type layer-type
+                         :data (to-core-matrix node-data [(m/ecount node-data)])}])))
+       (sort-by first)
+       (map second)
+       (remove #(= (:layer-type %) :Flatten))
+       vec))
+
+(def layer-type-map
+  (into {}
+   (mapv vec (partition 2
+                        [:Convolution2D :convolutional
+                         :Activation #{:relu :softmax}
+                         :MaxPooling2D :max-pooling
+                         :Dense :linear
+                         :Dropout :dropout]))))
+
+
+(defn output-types-differ?
+  [layer-types]
+  (->> layer-types
+       (map (fn [[cortex-type keras-type]]
+              (let [entry (layer-type-map keras-type)]
+                (when-not (and entry
+                               (if (set? entry)
+                                 (contains? entry cortex-type)
+                                 (= entry cortex-type)))
+                  [cortex-type keras-type]))))
+       (remove nil?)
+       seq))
+
 
 (defn load-combined-hdf5-file
   [fname]
   (resource/with-resource-context
     (let [model-file (hdf5/open-file fname)
-          file-data (hdf5-child-map model-file)
+          file-child-map (hdf5-child-map model-file)
           printer (fn [item]
                     (clojure.pprint/pprint item)
                     item)
-          src-desc (-> (:model_config file-data)
+          src-desc (-> (:model_config file-child-map)
                        hdf5/->clj
                        :data
                        first
                        (json/parse-string keyword)
                        model->simple-description)
-          weight-desc (load-weights src-desc model-file)]
-      weight-desc
-      )))
+
+          weight-desc (load-weights (mapv vector
+                                          src-desc
+                                          (desc/build-full-network-description src-desc))
+                                    model-file)
+          input-desc (first weight-desc)
+          input-shape (if (:output-width input-desc)
+                        [(:output-channels input-desc)
+                         (* (:output-height input-desc)
+                            (:output-width input-desc))]
+                        [(:output-size input-desc)])
+          file-data (if-let [input-data (get file-child-map :test_image)]
+                      (do
+                        (println "Using file input data")
+                        (:data (hdf5/->clj input-data)))
+                      (double-array (vec (repeat (reduce * input-shape) 1.0))))
+          input (to-core-matrix file-data input-shape)
+          layer-outputs (layer-output->ordered-data (:layer_outputs file-child-map))
+          type-map (vec (map vector
+                             (map :type (drop 1 weight-desc))
+                             (map :layer-type layer-outputs)))]
+      (when-let [verify-seq (seq (desc/build-and-verify-trained-network weight-desc))]
+        (throw (Exception. (format "Built items failed verification:\n %s" (vec verify-seq)))))
+      (when-not (= (count layer-outputs)
+                   (- (count weight-desc) 1))
+        (throw (Exception. (format "Layer output count mismatch: %s"
+                                   type-map))))
+      (when (output-types-differ? type-map)
+        (throw (Exception. (format "Layer output type mismatch %s %s"
+                                   type-map
+                                   (vec (output-types-differ? type-map))))))
+      {:model weight-desc
+       :input input
+       :layer-outputs (mapv :data layer-outputs)})))
