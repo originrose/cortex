7ac72c2f5b8e4f6154621a1bb5349756745d2c54
Chris Nuernberger <chris@thinktopic.com>
2016-10-20 16:18:52 -0600

    Attempting to get keras to build.

---
+++ datasets/build-and-deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+lein test && lein deploy clojars
---
+++ keras/build-and-deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker build -t cortex-keras-buildimg
+docker run --name buildimg --volume=`pwd`:/root/build cortex-keras-buildimg \
+       /bin/bash -c "cd /root/build && ./get-test-models.sh && lein test && lein deploy clojars"
