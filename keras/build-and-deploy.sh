8b04d6ab24bf80a04fafac9882ef09791712aede
Chris Nuernberger <chris@thinktopic.com>
2016-10-20 17:10:13 -0600

    Checking in another attempt at building this thing.

--- keras/build-and-deploy.sh
+++ keras/build-and-deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cp ../profiles.clj .
 docker build -t cortex-keras-buildimg .
 docker run --name buildimg --volume=`pwd`:/root/build cortex-keras-buildimg \
        /bin/bash -c "cd /root/build && ./get-test-models.sh && lein test && lein deploy clojars"
