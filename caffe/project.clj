1cfb96d07d1d466f8017ce6a68736d92e138829a
Chris Nuernberger <chris@thinktopic.com>
2016-11-01 19:22:50 +0000

    Initial empty new project for caffe support.

---
+++ caffe/project.clj
@@ -0,0 +1,6 @@
+(defproject caffe "0.1.0-SNAPSHOT"
+  :description "Caffe support for cortex."
+  :url "http://github.com/thinktopic/cortex"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
---
+++ caffe/src/caffe/core.clj
@@ -0,0 +1,6 @@
+(ns caffe.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
