ce5d19299337d273eb34542a5767d82b75f9be19
Chris Nuernberger <chris@thinktopic.com>
2016-11-01 16:17:33 -0600

    Small changes to core.

--- caffe/src/caffe/core.clj
+++ caffe/src/caffe/core.clj
@@ -1,7 +1,9 @@
 (ns caffe.core
   (:require [think.hdf5.core :as hdf5]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn hdf5-child-map
+  [node]
+  (into {} (map (fn [node-child]
+                  [(keyword (hdf5/get-name node-child))
+                   node-child])
+                (hdf5/get-children node))))
