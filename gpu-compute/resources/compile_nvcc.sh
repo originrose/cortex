654c7e57218214ceea01dc211c004e8a24c6f5b0
Chris Nuernberger <chris@thinktopic.com>
2016-11-04 10:10:04 -0600

    Attempting to bundle multiple kernel images into fatbin files.

--- gpu-compute/resources/compile_nvcc.sh
+++ gpu-compute/resources/compile_nvcc.sh
@@ -5,9 +5,13 @@ NVCC=nvcc
 # Could be compute_50, however that arch doesn't work on Kepler devices
 # see here for mroe info:
 # http://docs.nvidia.com/cuda/cuda-compiler-driver-nvcc/#virtual-architecture-feature-list
-ARCH=compute_30
 
 for i in `ls *.cu`
 do
-  $NVCC -fatbin -gencode arch=$ARCH,code=$ARCH $i
+    echo "Compiling $i"
+    $NVCC -fatbin $i \
+	  -gencode arch=compute_20,code=compute_20 \
+	  -gencode arch=compute_30,code=compute_30 \
+	  -gencode arch=compute_35,code=compute_35 \
+	  -gencode arch=compute_50,code=compute_50
 done
