e6b41f7664f8067bdce248351186e8023fae9cf9
Chris Nuernberger <chris@thinktopic.com>
2016-11-06 14:46:49 -0700

    Reverting back to cuda 7.5 for now.

--- gpu-compute/project.clj
+++ gpu-compute/project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.bytedeco.javacpp-presets/cuda "8.0-1.2"]
+                 [org.bytedeco.javacpp-presets/cuda "7.5-1.2"]
                  [thinktopic/compute "0.1.0-SNAPSHOT"]])
