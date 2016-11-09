0c1c2d5e0b10ca35b2b11f13061f6e4256204d84
Chris Nuernberger <chris@thinktopic.com>
2016-11-09 09:11:45 -0700

    Attempting to fix build.

--- caffe/build-and-deploy.sh
+++ caffe/build-and-deploy.sh
@@ -2,6 +2,5 @@
 
 cp ../profiles.clj .
 docker build -t cortex-keras-buildimg .
-docker run --name buildimg --volume=`pwd`:/root/build cortex-keras-buildimg \
-       -e GIT_IS_MASTER='$GIT_IS_MASTER' \
+docker run --name buildimg --volume=`pwd`:/root/build -e GIT_IS_MASTER='$GIT_IS_MASTER' cortex-keras-buildimg \
        /bin/bash -c "cd /root/build && ./get-test-models.sh && lein test && lein test && if [ \"$GIT_IS_MASTER\" == \"0\" ]; then lein deploy clojars; fi"
