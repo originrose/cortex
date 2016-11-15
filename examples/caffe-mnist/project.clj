ea14ca593be47e2c9fdae236bcc01953e02b01c1
Ben Kamphaus <ben.kamphaus@gmail.com>
2016-11-15 14:59:38 -0700

    [examples] add project.clj to caffe-mnist subdir from refactor

---
+++ examples/caffe-mnist/project.clj
@@ -0,0 +1,7 @@
+(defproject caffe-mnist "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [thinktopic/cortex "0.2.1-SNAPSHOT"]
+                 [thinktopic/cortex-datasets "0.3.0-SNAPSHOT"]
+                 [thinktopic/cortex-caffe "0.1.0-SNAPSHOT"]]
+  :main caffe-mnist.core
+)
