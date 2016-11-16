2e5e97e7cc3c6ea1a00221c8a89bb668f1d8cba8
Harold <hhausman@gmail.com>
2016-11-16 09:44:03 -0700

    Learning attenuation first cut (#33)
    * [util] Working around an exception coming from core.matrix:
    https://github.com/mikera/core.matrix/issues/302
    
    * [learning-attenuation] First cut at learning attenuation.
    
    This will eventually allow importing models, converting them to descriptions and then traning only subsets of the layers.

--- compute/src/think/compute/nn/description.clj
+++ compute/src/think/compute/nn/description.clj
@@ -93,9 +93,19 @@
   (let [sub-networks (mapv create-network branches)]
     (layers/split backend sub-networks input-size)))
 
+(defn description->module
+  [backend desc]
+  (let [new-module (create-module desc backend)
+        extra-keys (clojure.set/difference (set (keys desc)) (set (keys new-module)))]
+    (when new-module
+      (reduce (fn [eax k]
+                (assoc eax k (get desc k)))
+              new-module
+              extra-keys))))
+
 (defn create-network
   [built-descriptions backend batch-size]
-  (let [modules (filterv identity (map #(create-module % backend) built-descriptions))]
+  (let [modules (filterv identity (map (partial description->module backend) built-descriptions))]
     (cp/setup (layers/layer-list modules) batch-size)))
 
 (defn build-and-create-network
