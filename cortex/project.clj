1bff68b226dc8d33d98fdfa137ca030427385ba7
mikera <mike.r.anderson.13@gmail.com>
2016-11-22 17:11:21 +0800

    Update dependencies

--- cortex/project.clj
+++ cortex/project.clj
@@ -8,21 +8,21 @@
                  [org.clojure/test.check "0.9.0"]
                  [thinktopic/matrix.fressian "0.3.1"]
                  [net.mikera/vectorz-clj "0.45.0"]
-                 [net.mikera/core.matrix "0.56.0"]
-                 [net.mikera/clojure-utils "0.7.0"]
+                 [net.mikera/core.matrix "0.57.0"]
+                 [net.mikera/clojure-utils "0.7.1"]
                  [core.blas "1.0.2"]
                  [com.github.fommil.netlib/all "1.1.2" :extension "pom"]
                  [thinktopic/think.parallel "0.1.0"]]
 
   :java-source-paths ["java"]
 
-  :profiles {:dev {:dependencies [[net.mikera/cljunit "0.4.1"]  ;; allows JUnit testing
+  :profiles {:dev {:dependencies [[net.mikera/cljunit "0.6.0"]  ;; allows JUnit testing
                                   [criterium/criterium "0.4.4"] ;; benchmarking tool
                                   ] ;; alternate core.matrix implementation
                    :source-paths ["src" "test/cljc" "test/clj"]
                    :java-source-paths ["test/clj"]}
 
-             :test {:dependencies [[net.mikera/cljunit "0.4.0"]
+             :test {:dependencies [[net.mikera/cljunit "0.6.0"]
                                    [criterium/criterium "0.4.4"]
                                    [clatrix "0.5.0" :exclusions [net.mikera/core.matrix]]]
                     :source-paths ["src" "test/cljc" "test/cljs" "test/clj"]
