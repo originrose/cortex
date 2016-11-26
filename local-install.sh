c9596ab3b2c370600f7700dfcb1775178274ba21
Chris Nuernberger <chris@thinktopic.com>
2016-11-26 09:22:01 -0700

    Adding more libraries to a local cortex install

--- local-install.sh
+++ local-install.sh
@@ -4,4 +4,5 @@ cd cortex && lein install && cd ..
 cd datasets && lein install && cd ..
 cd compute && lein install && cd ..
 cd gpu-compute && lein install && cd ..
+cd suite && lein install && cd ..
 cd visualization && lein install && cd ..
