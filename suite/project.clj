c259542fc1e6d30123c24e4a16e154489a47ffbb
Chris Nuernberger <chris@thinktopic.com>
2016-11-30 15:08:42 -0700

    Upgrading think.image dependency

--- suite/project.clj
+++ suite/project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [thinktopic/gpu-compute "0.1.0-8.0-SNAPSHOT"]
-                 [thinktopic/think.image "0.4.1-SNAPSHOT"]
+                 [thinktopic/think.image "0.4.2"]
                  [com.taoensso/nippy "2.12.2"]])
