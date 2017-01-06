b824374d39d0c60506958b5ef87259595f36acb5
Chris Nuernberger <chris@thinktopic.com>
2017-01-06 09:52:37 -0700

    For now cortex is still cuda 7.5 based.  This will certainly change soon, however.

--- gpu-compute/project.clj
+++ gpu-compute/project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [thinktopic/compute "0.3.1-SNAPSHOT"]
-;                 [org.bytedeco.javacpp-presets/cuda "7.5-1.2"]
-                 [org.bytedeco.javacpp-presets/cuda "8.0-1.2"]
+                 [org.bytedeco.javacpp-presets/cuda "7.5-1.2"]
+;                 [org.bytedeco.javacpp-presets/cuda "8.0-1.2"]
                  ]
   :plugins [[lein-codox "0.10.2"]])
