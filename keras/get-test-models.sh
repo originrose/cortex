246ee1340cba58312acfe899c8a31942ac3301ff
Chris Nuernberger <chris@thinktopic.com>
2017-01-06 13:24:59 -0700

    Uploaded models and updated get script.

--- keras/get-test-models.sh
+++ keras/get-test-models.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 mkdir -p models
-wget https://s3-us-west-2.amazonaws.com/thinktopic.cortex/models/keras/mnist_combined.h5 -O models/mnist_combined.h5
+wget https://s3-us-west-2.amazonaws.com/thinktopic.cortex/models/keras/simple_mnist.json -O models/simple_mnist.json
+wget https://s3-us-west-2.amazonaws.com/thinktopic.cortex/models/keras/simple_mnist_output.h5 -O models/simple_mnist_output.h5
+wget https://s3-us-west-2.amazonaws.com/thinktopic.cortex/models/keras/simple_mnist.h5 -O models/simple_mnist.h5
