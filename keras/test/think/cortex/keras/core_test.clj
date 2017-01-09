da948095d5a9c2e491e0ac3eda6a66c02c5da183
Ben Kamphaus <ben.kamphaus@gmail.com>
2017-01-09 10:01:00 -0700

    [keras] starting to improve tests working from previous known bugs

--- keras/test/think/cortex/keras/core_test.clj
+++ keras/test/think/cortex/keras/core_test.clj
@@ -5,10 +5,25 @@
             [think.compute.nn.compute-execute :as ce]))
 
 
+(def simple_archf "models/simple_mnist.json")
+(def simple_weightf "models/simple_mnist.h5")
+(def simple_outf "models/simple_mnist_output.h5")
+
+
+(deftest match-padding-correct
+  "Verify that we get the correct padding value for same or valid padding."
+  (is (= [2 2] (keras/match-padding {:border_mode "same" :nb_col 5 :nb_row 5})))
+  (is (= [1 1] (keras/match-padding {:border_mode "same" :nb_col 3 :nb_row 3})))
+  (is (= [3 3] (keras/match-padding {:border_mode "same" :nb_col 7 :nb_row 7})))
+  (is (= [0 0] (keras/match-padding {:border_mode "valid" :nb_col 3 :nb_row 3})))
+  (is (= [1 2] (keras/match-padding {:padding [1 2]}))))
+
+
 (deftest verify-simple-mnist
   "This is a basic model which has no ambiguity introduced by uneven strides,
   where frameworks start to differ. A failure here indicates a very basic
-  problem in the Keras importer."
-  (is (keras/load-sidecar-and-verify "models/simple_mnist.json"
-                                     "models/simple_mnist.h5"
-                                     "models/simple_mnist_output.h5")))
+  problem in the Keras importer.
+
+  Model does, however, inclue Dropout, so requires handling in inference or 
+  trainin step so layer is not skipped."
+  (is (keras/load-sidecar-and-verify simple_archf simple_weightf simple_outf)))
