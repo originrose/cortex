71abe54071ea91788b460f1c3dcc22ecd2dad2f9
Kiran Karkera <shark8me@users.noreply.github.com>
2017-01-24 00:47:09 +0800

    works on CPU when --force-cpu false option is provided (#77)
    Agreed this looks great.  Tools.cli looks very useful.

--- examples/suite-classification/src/suite_classification/core.clj
+++ examples/suite-classification/src/suite_classification/core.clj
@@ -262,7 +262,7 @@ infinite sequence of maps, each map is one entry and all maps have the same keys
 
 
 (defn display-dataset-and-model
-  ([dataset]
+  ([dataset argmap]
    (let [initial-description initial-network
          data-display-atom (atom {})
          confusion-matrix-atom (atom {})]
@@ -271,10 +271,12 @@ infinite sequence of maps, each map is one entry and all maps have the same keys
                                                       initial-description)]
        (classification/reset-confusion-matrix confusion-matrix-atom mnist-observation->image
                                               dataset
-                                              (suite-train/evaluate-network
+                                              (apply suite-train/evaluate-network
                                                dataset
                                                loaded-data
-                                               :batch-type :cross-validation)))
+                                               (-> (merge argmap 
+                                                       {:batch-type :cross-validation})
+                                                   seq flatten))))
 
      (let [open-message
            (gate/open (atom
@@ -292,21 +294,21 @@ infinite sequence of maps, each map is one entry and all maps have the same keys
 
 
 (defn train-forever
-  []
+  [argmap]
   (let [dataset (if *run-from-nippy*
                   (create-nippy-dataset)
                   (create-dataset))
-        confusion-matrix-atom (display-dataset-and-model dataset)]
-    (classification/train-forever dataset mnist-observation->image
+        confusion-matrix-atom (display-dataset-and-model dataset argmap)]
+    (apply classification/train-forever dataset mnist-observation->image
                                   initial-network
-                                  :confusion-matrix-atom confusion-matrix-atom)))
-
+                                  (-> (merge argmap 
+                                         {:confusion-matrix-atom confusion-matrix-atom})
+                                         seq flatten))))
 
 (defn train-forever-uberjar
-  []
-  (with-bindings {#'*running-from-repl* false}
-    (train-forever)))
-
+  [argmap]
+  (with-bindings {#'*running-from-repl* (not (:live-updates? argmap))}
+    (train-forever argmap)))
 
 (defn label-one
   "Take an arbitrary image and label it."
--- examples/suite-classification/src/suite_classification/main.clj
+++ examples/suite-classification/src/suite_classification/main.clj
@@ -1,9 +1,20 @@
 (ns suite-classification.main
+  (:require  [clojure.tools.cli :refer  [parse-opts]])
   (:gen-class))
+
+(def cli-options
+  [["-f" "--force-gpu value" "Force GPU to be used"
+    :id :force-gpu?
+    :parse-fn #(Boolean/parseBoolean %)
+    :default true]
+   [ "-l" nil 
+    :long-opt "--live-updates"   
+    :id :live-updates?
+    :default false]])
 
 (defn -main
   [& args]
-  (require 'suite-classification.core)
-  (if (= "live-updates" (first args))
-    ((resolve 'suite-classification.core/train-forever))
-    ((resolve 'suite-classification.core/train-forever-uberjar))))
+  (let [argmap (parse-opts args cli-options)]
+    (require 'suite-classification.core)
+    ((resolve 'suite-classification.core/train-forever-uberjar) 
+     (:options argmap))))
