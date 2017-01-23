c8cfbb874d1be00b220f2c70638ee6b3a269eadf
Chris Nuernberger <chris@thinktopic.com>
2017-01-23 13:53:38 -0700

    Adding script to get original caffe models.

---
+++ caffe/get-mtcnn-face-detect.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget https://github.com/kpzhang93/MTCNN_face_detection_alignment/raw/master/code/codes/MTCNNv1/model/det1.caffemodel -O models/det1.caffemodel
+wget https://github.com/kpzhang93/MTCNN_face_detection_alignment/raw/master/code/codes/MTCNNv1/model/det1.prototxt -O models/det1.prototxt
+wget https://github.com/kpzhang93/MTCNN_face_detection_alignment/raw/master/code/codes/MTCNNv1/model/det2.caffemodel -O models/det2.caffemodel
+wget https://github.com/kpzhang93/MTCNN_face_detection_alignment/raw/master/code/codes/MTCNNv1/model/det2.prototxt -O models/det2.prototxt
+wget https://github.com/kpzhang93/MTCNN_face_detection_alignment/raw/master/code/codes/MTCNNv1/model/det3.caffemodel -O models/det3.caffemodel
+wget https://github.com/kpzhang93/MTCNN_face_detection_alignment/raw/master/code/codes/MTCNNv1/model/det3.prototxt -O models/det3.prototxt
