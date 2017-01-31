0dc7088ba40066499d97988e6979de6ebc16312b
Chris Nuernberger <chris@thinktopic.com>
2017-01-31 11:39:10 -0600

    Release 0.4.0

--- caffe/project.clj
+++ caffe/project.clj
@@ -1,11 +1,11 @@
-(defproject thinktopic/cortex-caffe "0.4.0-SNAPSHOT"
+(defproject thinktopic/cortex-caffe "0.4.0"
   :description "Caffe support for cortex."
   :url "http://github.com/thinktopic/cortex"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [thinktopic/hdf5 "0.1.2"]
-                 [thinktopic/compute "0.4.0-SNAPSHOT"]
+                 [thinktopic/compute "0.4.0"]
                  [instaparse "1.4.3"]
                  [com.taoensso/nippy "2.12.2"]]
   :plugins [[lein-codox "0.10.2"]])
