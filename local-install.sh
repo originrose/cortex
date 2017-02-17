9af01df28c4a543a60d978a99802e46c74249e91
Charles Gruenwald <charles@thinktopic.com>
2017-02-17 15:16:32 -0700

    [scripts] Move to subdir.

--- local-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd cortex && lein install && cd ..
-cd datasets && lein install && cd ..
-cd compute && lein install && cd ..
-cd gpu-compute && lein install && cd ..
-cd suite && lein install && cd ..
-cd visualization && lein install && cd ..
