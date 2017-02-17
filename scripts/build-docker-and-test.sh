4d3b8c4ea6a7cb0040baa6ab2326cb89c8e5d125
Charles Gruenwald <charles@thinktopic.com>
2017-02-17 15:36:27 -0700

    [docker] Move the dockerfile.

--- scripts/build-docker-and-test.sh
+++ scripts/build-docker-and-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -t cortex-buildimg .
+docker build -t cortex-buildimg -f scripts/Dockerfile
 docker run --rm --name buildimg --volume=`pwd`:/root/build -e LEIN_ROOT=1 cortex-buildimg \
        /bin/bash -c "cd /root/build && ./build-and-test.sh"
