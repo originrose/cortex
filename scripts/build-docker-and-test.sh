669dc9f072a9266af673dd57c876905605ab33f2
Charles Gruenwald <charles@thinktopic.com>
2017-02-18 10:17:47 -0700

    [docker] Update script that runs dockerfile.

--- scripts/build-docker-and-test.sh
+++ scripts/build-docker-and-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -t cortex-buildimg -f scripts/Dockerfile
+docker build -t cortex-buildimg scripts
 docker run --rm --name buildimg --volume=`pwd`:/root/build -e LEIN_ROOT=1 cortex-buildimg \
        /bin/bash -c "cd /root/build && ./build-and-test.sh"
