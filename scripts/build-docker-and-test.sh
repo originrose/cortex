ea7133280a5e4d5097e06499d64f53f0cb22d429
Charles Gruenwald <charles@thinktopic.com>
2017-02-18 12:57:07 -0700

    [tests] Changes to get tests to build in docker container.

--- scripts/build-docker-and-test.sh
+++ scripts/build-docker-and-test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 docker build -t cortex-buildimg scripts
-docker run --rm --name buildimg --volume=`pwd`:/root/build -e LEIN_ROOT=1 cortex-buildimg \
-       /bin/bash -c "cd /root/build && ./scripts/build-and-test.sh"
+docker run --rm --name buildimg              \
+    --volume=`pwd`:/root/build               \
+    -e LEIN_ROOT=1                           \
+    -e LD_LIBRARY_PATH=/usr/local/cuda/lib64 \
+    cortex-buildimg                          \
+    /bin/bash -c "cd /root/build && ./scripts/build-and-test.sh"
