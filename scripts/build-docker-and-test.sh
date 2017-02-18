adbee7e32b64f6c95517268b5b8bd8f111864888
Charles Gruenwald <charles@thinktopic.com>
2017-02-18 10:23:01 -0700

    [scripts] The build-and-test.sh file is in the scripts dir.

--- scripts/build-docker-and-test.sh
+++ scripts/build-docker-and-test.sh
@@ -2,4 +2,4 @@
 
 docker build -t cortex-buildimg scripts
 docker run --rm --name buildimg --volume=`pwd`:/root/build -e LEIN_ROOT=1 cortex-buildimg \
-       /bin/bash -c "cd /root/build && ./build-and-test.sh"
+       /bin/bash -c "cd /root/build && ./scripts/build-and-test.sh"
