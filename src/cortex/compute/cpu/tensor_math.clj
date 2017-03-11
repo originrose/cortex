6372b52471555b77ff0b1336c06904d3ce0b89b8
Chris Nuernberger <chris@thinktopic.com>
2017-03-10 17:59:05 -0700

    Finally have the large combinatorial macros working.

--- src/cortex/compute/cpu/tensor_math.clj
+++ src/cortex/compute/cpu/tensor_math.clj
@@ -4,7 +4,9 @@
             [cortex.tensor.math :as tm]
             [cortex.tensor.index-system :as is]
             [clojure.math.combinatorics :as combo]
-            [cortex.compute.cpu.driver :as cpu-driver]))
+            [cortex.compute.cpu.driver :as cpu-driver]
+            [think.parallel.core :as parallel])
+  (:import [cortex.compute.cpu.driver CPUStream]))
 
 
 (set! *unchecked-math* :warn-on-boxed)
@@ -87,11 +89,14 @@
     (adjustment-calc
      ~adjustment
      (elems-per-idx-calc
-      index-strategy-calc ~elems-per-idx ~'elements-per-idx ~system-type ~'constant ~'length ~'indexes)
+      index-strategy-calc
+      ~elems-per-idx ~'elements-per-idx
+      ~system-type ~'constant ~'length ~'indexes)
      ~'idx-numerator ~'idx-denominator)
     ~'num-columns ~'column-stride))
 
 
+;;Need the interface to get correct type hinting to avoid boxing/unboxing every index.
 (definterface ElemIdxToAddressFunction
   (^long idx_to_address [^long arg]))
 
@@ -111,7 +116,8 @@
       (proxy [ElemIdxToAddressFunction] []
         (idx_to_address [~'elem-idx]
           (let [~'elem-idx (long ~'elem-idx)]
-            (combine-calculations ~elems-per-idx ~system-type ~adjustment ~address)))))))
+           (combine-calculations ~elems-per-idx ~system-type ~adjustment ~address)))))))
+
 
 (defn- generate-combinations
   []
@@ -129,11 +135,111 @@
           (generate-combinations)))
 
 
-(def combination-map
-  (->> (generate-all-address-functions)
-       (into {})))
+(defonce combination-map
+  (memoize
+   (fn []
+     (->> (generate-all-address-functions)
+          (into {})))))
 
 
 (defn get-elem-idx->address
   ^ElemIdxToAddressFunction [index-system]
-  ((get combination-map (classify-index-system index-system)) index-system))
+  ((get (combination-map) (classify-index-system index-system)) index-system))
+
+
+(defmacro assign-constant-impl
+  [view-type view-cast-fn _ dtype-cast-fn]
+  `(vector
+    (dtype/get-datatype (~dtype-cast-fn 0))
+    (fn [buffer# index-system# value# n-elems#]
+      (let [n-elems# (long n-elems#)
+            buffer# (~view-cast-fn buffer#)
+            idx->address# (get-elem-idx->address index-system#)
+            value# (~dtype-cast-fn value#)]
+        (parallel/parallel-for
+         idx# n-elems#
+         (dtype/v-aset buffer# (.idx_to_address idx->address# idx#) value#))))))
+
+
+(defonce assign-constant-map
+  (memoize
+   (fn []
+     (->> (marshal/array-view-iterator assign-constant-impl)
+          (into {})))))
+
+
+(defmacro datatype->view-cast-fn
+  [dtype buf]
+  (condp = dtype
+    :byte `(marshal/as-byte-array-view ~buf)
+    :short `(marshal/as-short-array-view ~buf)
+    :int `(marshal/as-int-array-view ~buf)
+    :long `(marshal/as-long-array-view ~buf)
+    :float `(marshal/as-float-array-view ~buf)
+    :double `(marshal/as-double-array-view ~buf)))
+
+(defmacro datatype->cast-fn
+  [dtype val]
+  (condp = dtype
+    :byte `(byte ~val)
+    :short `(short ~val)
+    :int `(int ~val)
+    :long `(long ~val)
+    :float `(float ~val)
+    :double `(double ~val)))
+
+
+(defn generate-datatype-combinations
+  []
+  (let [all-dtypes [:byte :short :int :long :float :double]]
+    (for [lhs all-dtypes
+          rhs all-dtypes]
+      [lhs rhs])))
+
+
+(defmacro marshalling-assign-fn
+  [lhs-dtype rhs-dtype]
+  `(fn [dest# dest-idx-sys#
+        src# src-idx-sys#
+        n-elems#]
+     (let [dest# (datatype->view-cast-fn ~lhs-dtype dest#)
+           src# (datatype->view-cast-fn ~rhs-dtype src#)
+           dest-idx->address# (get-elem-idx->address dest-idx-sys#)
+           src-idx->address# (get-elem-idx->address src-idx-sys#)
+           n-elems# (long n-elems#)]
+       (parallel/parallel-for
+        idx# n-elems#
+        (dtype/v-aset dest# (dest-idx->address# idx#)
+                      (datatype->cast-fn
+                       ~lhs-dtype
+                       (dtype/v-aget src# (src-idx->address# idx#))))))))
+
+
+(defmacro generate-all-marshalling-assign-fns
+  []
+  `~(mapv (fn [[lhs-dtype rhs-dtype :as combo]]
+            `[~combo (marshalling-assign-fn ~lhs-dtype ~rhs-dtype)])
+          (generate-datatype-combinations)))
+
+
+(defonce assign!-map
+  (memoize
+   (fn []
+     (->> (generate-all-marshalling-assign-fns)
+          (into {})))))
+
+
+(extend-type CPUStream
+  tm/TensorMath
+  (assign-constant! [stream buffer index-system value n-elems]
+    (cpu-driver/with-stream-dispatch stream
+      ((get (assign-constant-map (dtype/get-datatype buffer)))
+       buffer index-system value n-elems)))
+  (assign! [stream
+            dest dest-idx-sys dest-ecount
+            src src-idx-sys src-ecount]
+    (cpu-driver/with-stream-dispatch stream
+      ((get (assign!-map [(dtype/get-datatype dest) (dtype/get-datatype src)]))
+       dest dest-idx-sys
+       src src-idx-sys
+       (max (long dest-ecount) (long src-ecount))))))
