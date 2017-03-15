6cb917a90c1b4c396041c3f7f557c479220420f0
Chris Nuernberger <chris@thinktopic.com>
2017-03-15 15:05:42 -0600

    Release 0.9.0

--- experiment/project.clj
+++ experiment/project.clj
@@ -1,8 +1,7 @@
-(defproject experiment "0.9.0-SNAPSHOT"
+(defproject experiment "0.9.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [thinktopic/cortex "0.9.0-SNAPSHOT"]])
-
+                 [thinktopic/cortex "0.9.0"]])
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thinktopic/cortex "0.9.0-SNAPSHOT"
+(defproject thinktopic/cortex "0.9.0"
   :description "A neural network toolkit for Clojure."
   :url "https://github.com/thinktopic/cortex"
   :license {:name "Eclipse Public License"
