0e5b233f147388908616ba0134935e0f4c060ba9
Chris Nuernberger <chris@thinktopic.com>
2017-03-15 15:14:32 -0600

    Finished release with some hacks.

--- experiment/project.clj
+++ experiment/project.clj
@@ -1,4 +1,4 @@
-(defproject experiment "0.9.0"
+(defproject thinktopic/experiment "0.9.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
--- scripts/clojars-deploy.sh
+++ scripts/clojars-deploy.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-PROJECTS="cortex datasets compute gpu-compute caffe keras optimise suite"
+PROJECTS=". experiment"
 
 for proj in $PROJECTS; do
     pushd $proj
