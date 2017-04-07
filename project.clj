3cf64432c8ba118489b45f087cf936e9ae96c056
Chris Nuernberger <chris@thinktopic.com>
2017-04-07 08:37:15 -0600

    Removed spurious dependencies.

--- project.clj
+++ project.clj
@@ -5,23 +5,17 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
 
-                 [net.mikera/core.matrix "0.57.0"]
-                 [net.mikera/vectorz-clj "0.45.0"]
+                 [net.mikera/core.matrix "0.58.0"]
+                 [net.mikera/vectorz-clj "0.46.0"]
                  [com.github.fommil.netlib/all "1.1.2" :extension "pom"]
-                 [com.indeed/util-mmap "1.0.20"]
-                 [com.github.ben-manes.caffeine/caffeine "2.3.1"]
-                 [com.taoensso/nippy "2.12.2"]
-
+                 [com.taoensso/nippy "2.13.0"]
                  ;; Change the following dep to depend on different versions of CUDA
                  ;[org.bytedeco.javacpp-presets/cuda "7.5-1.2"]
                  [org.bytedeco.javacpp-presets/cuda "8.0-1.2"]
-
                  ;;The dataset abstraction uses parallel and optionally resource management.
                  [thinktopic/think.parallel "0.3.5"]
                  [thinktopic/think.datatype "0.3.10"]
-
-                 [thinktopic/resource "1.1.0"]
-                 [thinktopic/think.image "0.4.6"]]
+                 [thinktopic/resource "1.1.0"]]
 
   :java-source-paths ["java"]
 
