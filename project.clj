f0563a3b229cca1dd2996b9fda5efd6a67603f40
Chris Nuernberger <chris@thinktopic.com>
2017-04-18 14:27:05 -0600

    Release 0.9.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thinktopic/cortex "0.9.3-SNAPSHOT"
+(defproject thinktopic/cortex "0.9.3"
   :description "A neural network toolkit for Clojure."
   :url "https://github.com/thinktopic/cortex"
   :license {:name "Eclipse Public License"
