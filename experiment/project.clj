2d817e5e51d7735265cf64cba6dec9bc2c3f1736
Chris Nuernberger <chris@thinktopic.com>
2017-04-23 09:20:27 -0600

    Making dependency mt also.

--- experiment/project.clj
+++ experiment/project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [thinktopic/cortex "0.9.4-SNAPSHOT"]])
+                 [thinktopic/cortex "0.9.4-mt-SNAPSHOT"]])
