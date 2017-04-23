7154c81fa20c8c4260be734537a72099ecb7d8f3
Chris Nuernberger <chris@thinktopic.com>
2017-04-23 09:15:02 -0600

    Changed project version to test out releases.

--- experiment/project.clj
+++ experiment/project.clj
@@ -1,7 +1,7 @@
-(defproject thinktopic/experiment "0.9.4-SNAPSHOT"
+(defproject thinktopic/experiment "0.9.4-mt-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [thinktopic/cortex "0.9.4-SNAPSHOT"]])
+                 [thinktopic/cortex "0.9.4-mt-SNAPSHOT"]])
