583705c7a26dc780b6e4c2d719e66373c96df27e
Charles <charles@thinktopic.com>
2017-04-24 11:50:33 -0700

    [build] Pull the docker container instead of building it. Cache deps. (#148)
    * [build] Pull the docker container instead of building it. Cache deps.
    
    * [build] Don't include nvidia in the cortex-build image since it isn't needed.
    
    * [importers] Reduce output while testing.

--- importers/caffe/get-test-models.sh
+++ importers/caffe/get-test-models.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mkdir models
-wget https://s3-us-west-2.amazonaws.com/thinktopic.cortex/models/caffe/mnist.h5 -O models/mnist.h5
+mkdir -p models
+wget -q https://s3-us-west-2.amazonaws.com/thinktopic.cortex/models/caffe/mnist.h5 -O models/mnist.h5
--- scripts/build-and-deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e
-
-lein with-profile cpu-only test && lein install
