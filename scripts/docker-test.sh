4be05e23dcb058a5042436ba24b3bc0f0af54652
Charles Gruenwald <charles@thinktopic.com>
2017-04-24 12:33:09 -0600

    [build] Pull the docker container instead of building it. Cache deps.

--- scripts/docker-test.sh
+++ scripts/docker-test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-docker build -t cortex-buildimg scripts
+docker pull thinktopic/cortex-build
 docker run --rm --name buildimg              \
     --volume=`pwd`:/root/build               \
+    --volume=$HOME/.m2:/root/.m2             \
     -e LEIN_ROOT=1                           \
     -e LD_LIBRARY_PATH=/usr/local/cuda/lib64 \
-    cortex-buildimg                          \
+    thinktopic/cortex-build                  \
     /bin/bash -c "cd /root/build && ./scripts/build-and-test.sh"
