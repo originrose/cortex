2bdcb676f8c0b9213d2080d6d7ba018a4a325342
Chris Nuernberger <chris@thinktopic.com>
2017-04-26 09:30:13 -0600

    Back to snap.

--- examples/visualization/project.clj
+++ examples/visualization/project.clj
@@ -1,4 +1,4 @@
-(defproject thinktopic/cortex-visualization "0.9.5"
+(defproject thinktopic/cortex-visualization "0.9.5-SNAPSHOT"
   :description "Visualization library to aid in neural net training"
   :url "http://cortex.thinktopic.com"
   :license {:name "Eclipse Public License"
@@ -8,5 +8,5 @@
                  [incanter/incanter-core "1.5.7"]
                  [incanter/incanter-charts "1.5.7"]
                  [thinktopic/think.image "0.4.2"]
-                 [thinktopic/cortex "0.9.5"]]
+                 [thinktopic/cortex "0.9.5-SNAPSHOT"]]
   :plugins [[lein-codox "0.10.2"]])
