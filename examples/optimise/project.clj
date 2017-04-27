41dbe866ce1a77bd26731e7e9db675e6b799655c
Chris Nuernberger <chris@thinktopic.com>
2017-04-27 15:31:52 -0600

    Setting to correct snapshot version.

--- examples/optimise/project.clj
+++ examples/optimise/project.clj
@@ -1,4 +1,4 @@
-(defproject thinktopic/cortex-optimise "0.9.5-SNAPSHOT"
+(defproject thinktopic/cortex-optimise "0.9.6-SNAPSHOT"
   :description "General purpose optimization framework"
   :url "http://github.com/thinktopic/cortex"
   :license {:name "Eclipse Public License"
@@ -8,7 +8,7 @@
                       :machine-learning :exploratory :mathematica
                       :repl]}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [thinktopic/cortex "0.9.5-SNAPSHOT"]
+                 [thinktopic/cortex "0.9.6-SNAPSHOT"]
                  [net.mikera/vectorz-clj "0.45.0"]
                  [net.mikera/core.matrix "0.57.0"]
                  [thinktopic/lazy-map "0.1.0"]]
