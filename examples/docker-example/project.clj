ce5a34324a3ee8d7ef3a408956d71c61f43e8dce
Chris Nuernberger <chris@thinktopic.com>
2017-05-03 14:59:13 -0600

    Release 0.9.7

--- examples/docker-example/project.clj
+++ examples/docker-example/project.clj
@@ -1,7 +1,7 @@
-(defproject docker "0.9.7-SNAPSHOT"
+(defproject docker "0.9.7"
   :description "A simple example of how to run a cortex application in a docker container."
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [thinktopic/cortex "0.9.7-SNAPSHOT"]
+                 [thinktopic/cortex "0.9.7"]
                  [org.bytedeco.javacpp-presets/cuda "8.0-1.2"]]
 
   :main docker-example.core
