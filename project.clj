c5e3db2084cb164c9d6bccb1dbfb9855163b3f17
Chris Nuernberger <chris@thinktopic.com>
2017-05-03 13:15:31 -0600

    Upgrading dependencies.

--- project.clj
+++ project.clj
@@ -4,9 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-
-                 [net.mikera/core.matrix "0.58.0"]
-                 [net.mikera/vectorz-clj "0.46.0"]
+                 [thinktopic/think.datatype "0.3.11"]
                  [com.github.fommil.netlib/all "1.1.2" :extension "pom"]
                  [com.taoensso/nippy "2.13.0"]
                  ;; Change the following dep to depend on different versions of CUDA
@@ -14,7 +12,6 @@
                  [org.bytedeco.javacpp-presets/cuda "8.0-1.2"]
                  ;;The dataset abstraction uses parallel and optionally resource management.
                  [thinktopic/think.parallel "0.3.6"]
-                 [thinktopic/think.datatype "0.3.10"]
                  [thinktopic/think.resource "1.2.1"]]
 
   :java-source-paths ["java"]
