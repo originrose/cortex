b22c23ad9a5b842eefec3cfc9104bc48dd1ef6d4
Chris Nuernberger <chris@thinktopic.com>
2017-06-08 16:56:31 -0600

    Release 0.9.9

--- importers/model-upgrader/project.clj
+++ importers/model-upgrader/project.clj
@@ -1,4 +1,4 @@
-(defproject model-upgrader "0.9.9-SNAPSHOT"
+(defproject model-upgrader "0.9.9"
   :description "Upgrade a cortex model to the most recent version."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
