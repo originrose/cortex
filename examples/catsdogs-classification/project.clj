58c366840f3bf6c541642d78e3acaf0ff51032d6
Nicolas Modrzyk <hellonico@gmail.com>
2017-06-16 23:35:04 +0900

    Adding cats dogs classification example (#186)
    * Adding cats dogs classification example
    
    * add build.boot to sample

---
+++ examples/catsdogs-classification/project.clj
@@ -0,0 +1,24 @@
+(defproject mnist-classification "0.9.9-SNAPSHOT"
+  :description "An example of using experiment/classification on mnist."
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [thinktopic/experiment "0.9.9"]
+                 [org.clojure/tools.cli "0.3.5"]
+
+                 ; to manipulate images
+                 [thinktopic/think.image "0.4.8"]
+                 ; [net.mikera/imagez "0.12.0"]
+                 ;;If you need cuda 8...
+                 [org.bytedeco.javacpp-presets/cuda "8.0-1.2"]
+                 ;;If you need cuda 7.5...
+                 ;;[org.bytedeco.javacpp-presets/cuda "7.5-1.2"]
+                 ]
+
+  ; :main mnist-classification.main
+  ; :aot [mnist-classification.main]
+  :jvm-opts ["-Xmx2000m"]
+  :uberjar-name "classify-example.jar"
+
+  :clean-targets ^{:protect false} [:target-path
+                                    "figwheel_server.log"
+                                    "resources/public/out/"
+                                    "resources/public/js/app.js"])
