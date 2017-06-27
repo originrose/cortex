a145d4b8b502ba806515c634c11c4eb1b2850d22
Chris Nuernberger <chris@thinktopic.com>
2017-06-27 16:04:55 -0600

    Trying to make libhdf5 work on ubuntu 16.04

--- importers/caffe/project.clj
+++ importers/caffe/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [thinktopic/hdf5 "0.2.0"]
+                 [thinktopic/hdf5 "0.2.1"]
                  [thinktopic/cortex "0.9.11-SNAPSHOT"]
                  [instaparse "1.4.3"]
                  [com.taoensso/nippy "2.12.2"]]
