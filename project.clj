deaab6be8fa47856c73c0809e2a1e45068536fd4
Chris Nuernberger <chris@thinktopic.com>
2017-08-29 11:08:08 -0600

    Another dependency sweep.

--- project.clj
+++ project.clj
@@ -4,14 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
-                 [thinktopic/think.datatype "0.3.16"]
+                 [thinktopic/think.datatype "0.3.17"]
                  [com.github.fommil.netlib/all "1.1.2" :extension "pom"]
                  [com.taoensso/nippy "2.13.0"]
                  ;; Change the following dep to depend on different versions of CUDA
                  ;[org.bytedeco.javacpp-presets/cuda "7.5-1.2"]
                  [org.bytedeco.javacpp-presets/cuda "8.0-1.2"]
-                 [thinktopic/think.parallel "0.3.6"]
-                 [thinktopic/think.resource "1.2.1"]
                  [thinktopic/think.parallel "0.3.7"]
                  [thinktopic/think.resource "1.2.1"]
                  [org.clojure/math.combinatorics "0.1.4"]]
