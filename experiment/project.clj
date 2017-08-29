573388b4c322107ff79c96cdecd7206430ecb779
Chris Nuernberger <chris@thinktopic.com>
2017-08-29 11:37:08 -0600

    Resolving more deps.

--- experiment/project.clj
+++ experiment/project.clj
@@ -1,15 +1,16 @@
 (defproject thinktopic/experiment "0.9.12-SNAPSHOT"
   :description "A higher-level library for performing experiments with cortex."
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [org.clojure/clojurescript "1.9.854"]
                  [thinktopic/cortex "0.9.12-SNAPSHOT"]
                  [thinktopic/think.image "0.4.16"]
                  [org.shark8me/tfevent-sink "0.1.3"]
                  ;;Default way of displaying anything is a web page.
                  ;;Because if you want to train on aws (which you should)
                  ;;you need to get simple servers up and running easily.
-                 [thinktopic/think.gate "0.1.5"]]
+                 [thinktopic/think.gate "0.1.6"]]
 
-  :plugins [[lein-cljsbuild "1.1.5"]
+  :plugins [[lein-cljsbuild "1.1.7"]
             [lein-garden "0.3.0"]]
 
   :garden {:builds [{:id "dev"
