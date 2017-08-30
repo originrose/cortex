7eef0cf0139412cca8fd8d0e2774dfd116ce718f
Chris Nuernberger <chris@thinktopic.com>
2017-08-30 09:00:41 -0600

    Release 0.9.12

--- examples/resnet-retrain/project.clj
+++ examples/resnet-retrain/project.clj
@@ -1,9 +1,9 @@
-(defproject resnet-retrain "0.1.0-SNAPSHOT"
+(defproject resnet-retrain "0.9.12"
   :description "Retrain resnet50 for aerial imagery"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [thinktopic/experiment "0.9.12-SNAPSHOT"]
+                 [thinktopic/experiment "0.9.12"]
                  [thinktopic/think.image "0.4.12"]]
   :uberjar-name "resnet-retrain.jar"
   :main resnet-retrain.core)
--- examples/xor-mlp/project.clj
+++ examples/xor-mlp/project.clj
@@ -1,4 +1,4 @@
-(defproject xor-mlp "0.0.1-SNAPSHOT"
+(defproject xor-mlp "0.9.12"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [thinktopic/experiment "0.9.12-SNAPSHOT"]]
+                 [thinktopic/experiment "0.9.12"]]
   :repl-options {:init-ns xor-mlp.core})
