faeaac13c15dc8225ff7c44d038616e94ef5a9b6
Chris Nuernberger <chris@thinktopic.com>
2017-09-11 13:20:20 -0600

    back to snap.

--- examples/xor-mlp/project.clj
+++ examples/xor-mlp/project.clj
@@ -1,4 +1,4 @@
-(defproject xor-mlp "0.9.15"
+(defproject xor-mlp "0.9.16-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [thinktopic/experiment "0.9.15"]]
+                 [thinktopic/experiment "0.9.16-SNAPSHOT"]]
   :repl-options {:init-ns xor-mlp.core})
