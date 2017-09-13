c3b3dbc28a595067e285077ccc95ad77333cc039
Chris Nuernberger <chris@thinktopic.com>
2017-09-13 09:43:28 -0600

    Release 0.9.17

--- examples/xor-mlp/project.clj
+++ examples/xor-mlp/project.clj
@@ -1,4 +1,4 @@
-(defproject xor-mlp "0.9.17-SNAPSHOT"
+(defproject xor-mlp "0.9.17"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [thinktopic/experiment "0.9.17-SNAPSHOT"]]
+                 [thinktopic/experiment "0.9.17"]]
   :repl-options {:init-ns xor-mlp.core})
