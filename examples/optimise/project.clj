71c887e80384733381a54663114ab3cc50cef975
Chris Nuernberger <chris@thinktopic.com>
2017-09-13 09:44:24 -0600

    Back to snap.

--- examples/optimise/project.clj
+++ examples/optimise/project.clj
@@ -1,4 +1,4 @@
-(defproject thinktopic/cortex-optimise "0.9.17"
+(defproject thinktopic/cortex-optimise "0.9.18-SNAPSHOT"
   :description "General purpose optimization framework"
   :url "http://github.com/thinktopic/cortex"
   :license {:name "Eclipse Public License"
@@ -8,7 +8,7 @@
                       :machine-learning :exploratory :mathematica
                       :repl]}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
-                 [thinktopic/cortex "0.9.17"]
+                 [thinktopic/cortex "0.9.18-SNAPSHOT"]
                  [net.mikera/vectorz-clj "0.45.0"]
                  [net.mikera/core.matrix "0.57.0"]
                  [thinktopic/lazy-map "0.1.0"]]
