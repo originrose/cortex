4ca710424d4c8cfa285e8cce34707bbde893cdc3
Chris Nuernberger <chris@thinktopic.com>
2017-09-26 11:25:28 -0600

    Back to snap.

--- examples/resnet-retrain/project.clj
+++ examples/resnet-retrain/project.clj
@@ -1,8 +1,8 @@
-(defproject resnet-retrain "0.9.20"
+(defproject resnet-retrain "0.9.21-SNAPSHOT"
   :description "Retrain resnet50 for aerial imagery"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [thinktopic/experiment "0.9.20"]]
+                 [thinktopic/experiment "0.9.21-SNAPSHOT"]]
   :uberjar-name "resnet-retrain.jar"
   :main resnet-retrain.core)
--- examples/xor-mlp/project.clj
+++ examples/xor-mlp/project.clj
@@ -1,4 +1,4 @@
-(defproject xor-mlp "0.9.20"
+(defproject xor-mlp "0.9.21-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [thinktopic/experiment "0.9.20"]]
+                 [thinktopic/experiment "0.9.21-SNAPSHOT"]]
   :repl-options {:init-ns xor-mlp.core})
