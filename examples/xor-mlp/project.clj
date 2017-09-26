006eed22663646c223208f24343767ec8b06a1e3
Chris Nuernberger <chris@thinktopic.com>
2017-09-26 11:25:13 -0600

    Release 0.9.20

--- examples/xor-mlp/project.clj
+++ examples/xor-mlp/project.clj
@@ -1,4 +1,4 @@
-(defproject xor-mlp "0.9.20-SNAPSHOT"
+(defproject xor-mlp "0.9.20"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [thinktopic/experiment "0.9.20-SNAPSHOT"]]
+                 [thinktopic/experiment "0.9.20"]]
   :repl-options {:init-ns xor-mlp.core})
--- importers/model-upgrader/project.clj
+++ importers/model-upgrader/project.clj
@@ -1,4 +1,4 @@
-(defproject model-upgrader "0.9.20-SNAPSHOT"
+(defproject model-upgrader "0.9.20"
   :description "Upgrade a cortex model to the most recent version."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
